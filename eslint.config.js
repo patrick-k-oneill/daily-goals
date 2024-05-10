348156efce92bf804017c4b230c744a98548c37a
Patrick O'Neill <pwneill@outlook.com>
2024-05-10 19:05:16 -0400

    tsc upgrade

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,13 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  extends: [
+    "eslint:recommended",
+    "tseslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "prettier",
+  ],
   rules: {
-    'vue/no-unused-vars': 'error',
+    "vue/no-unused-vars": "error",
   },
-}
+};
